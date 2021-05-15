66fe0b0b6764cde0b8f81a4a1f51f922104d0885
replacetr <48390814+replacetr@users.noreply.github.com>
2021-05-15 22:40:31 +0800

    logg

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,7 @@
 class ApplicationJob < ActiveJob::Base
+    queue_as = default
+
+    def perform()
+        print ("sdfasdfasf")
+
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module MyTest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in your application.]
+    config.logger = Logger.new(STDOUT)
+    config.logger = Log4r::Logger.new("Application Log")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
