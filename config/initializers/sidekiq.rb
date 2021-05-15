297feb537c07e15e7cb4c1a9c46128015d3877bc
replacetr <48390814+replacetr@users.noreply.github.com>
2021-05-16 02:12:43 +0800

    sidekiq cron log

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
 Sidekiq.configure_server do |config|
     config.redis = { url: "redis://localhost:6379/0" }
     schedule_file = "config/schedule.yml"
-    config.logger.level = Logger::INFO
     if File.exist?(schedule_file)
       Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
     end
