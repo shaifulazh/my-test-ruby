676a0df55b194b92a84ae43d96970e606a6b3238
replacetr <48390814+replacetr@users.noreply.github.com>
2021-05-15 23:28:35 +0800

    buttonm

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  require 'sidekiq/web'
+  mount Sidekiq::Web=> '/sidekiq'
   get "/home", to: "home#index" 
 end
